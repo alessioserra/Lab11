3ed1b60b0cd20976c66b8096060118f13a8f1f60
zierp <zierp@DESKTOP-RURJMSA>
2019-05-29 15:18:08 +0200

    Simulazione implementata

---
+++ src/it/polito/tdp/bar/model/Tavolo.java
@@ -0,0 +1,15 @@
+package it.polito.tdp.bar.model;
+
+public class Tavolo {
+	
+	private int numeroPosti;
+
+	public Tavolo(int numeroPosti) {
+		this.numeroPosti = numeroPosti;
+	}
+
+	public int getNumeroPosti() {
+		return numeroPosti;
+	}
+	
+}
