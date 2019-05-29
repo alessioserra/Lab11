9301f2b592ac598968d08b005a2bcbf6196efa32
zierp <zierp@DESKTOP-RURJMSA>
2019-05-29 12:03:40 +0200

    interfaccia grafica e classi create

--- src/it/polito/tdp/bar/BarController.java
+++ src/it/polito/tdp/bar/BarController.java
@@ -1,5 +1,43 @@
 package it.polito.tdp.bar;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.bar.model.Simulatore;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class BarController {
 	
+	private Simulatore simulatore;
+	
+	public void setSimulatore(Simulatore simulatore) {
+		this.simulatore=simulatore;
+	}
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private Button handleSimula;
+
+    @FXML
+    void doSimula(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Bar.fxml'.";
+        assert handleSimula != null : "fx:id=\"handleSimula\" was not injected: check your FXML file 'Bar.fxml'.";
+
+    }
 }
